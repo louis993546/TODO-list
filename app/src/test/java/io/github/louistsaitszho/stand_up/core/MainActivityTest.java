35058d75cb69acee8a1492ccbc66d75adcea7cf7
Louis Tsai <louis993546@gmail.com>
2019-07-29 18:51:48 +0200

    Add bare-minimum AndroidX-style Robolectric test

--- app/src/test/java/io/github/louistsaitszho/stand_up/core/MainActivityTest.java
+++ app/src/test/java/io/github/louistsaitszho/stand_up/core/MainActivityTest.java
@@ -1,9 +1,32 @@
 package io.github.louistsaitszho.stand_up.core;
 
-import org.junit.runner.RunWith;
-import org.robolectric.RobolectricTestRunner;
+import android.app.Activity;
+import android.view.View;
 
-@RunWith(RobolectricTestRunner.class)
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+
+import io.github.louistsaitszho.stand_up.R;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
+
+    @Test
+    public void test() throws Exception {
+        Activity activity = Robolectric.setupActivity(MainActivity.class);
+
+        FloatingActionButton fab = activity.findViewById(R.id.fab_add_task);
+
+        assertNotNull(fab);
+        assertEquals(fab.getVisibility(), View.VISIBLE);
+    }
 
 }
